e1430c6f358189511704b588ccf9df4a859e7764
Igor Barbosa <igorbarbosa.p@gmail.com>
2015-08-03 20:24:44 -0300

    Fixing status for events and courses controllers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,13 +3,11 @@ class CoursesController < ApplicationController
 
   def index
     collection = @adapter.collection
-
-    render json: collection[:body], status: collection[:status]
+    render json: collection[:body], status: collection[:code]
   end
 
   def show
     member = @adapter.member(params[:id])
-
     render json: member[:body], status: member[:status]
   end
 
