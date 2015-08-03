6708d6fb20c46d0fecca47f5536d081126fee36c
Igor Barbosa <igorbarbosa.p@gmail.com>
2015-08-03 18:38:39 -0300

    Refactoring event strategy spec

--- spec/models/conexao_uff/event_strategy_spec.rb
+++ spec/models/conexao_uff/event_strategy_spec.rb
@@ -7,33 +7,32 @@ RSpec.describe ConexaoUff::EventStrategy, :type => :model do
     expect(strategy).to respond_to(:all)
   end
 
+  let!(:events) do
+    VCR.use_cassette('model/events') { strategy.all }
+  end
+
   describe '#all' do
     it 'should list the events' do
-      VCR.use_cassette 'model/events' do
-        events = strategy.all
         expect(events.first).to have_key(:system)
         expect(events.first).to have_key(:system_id)
         expect(events.first).to have_key(:name)
         expect(events.first).to have_key(:info)
-      end
     end
   end
 
   describe '#find' do
+    let(:event) do
+      VCR.use_cassette('model/event') { strategy.find(events.first[:system_id]) }
+    end
+
     it 'should find an event' do
-      events = VCR.use_cassette 'model/events' do
-        strategy.all
-      end
-      VCR.use_cassette 'model/event' do
-        event = strategy.find(events.first[:system_id])
-        expect(event).to have_key(:system)
-        expect(event).to have_key(:system_id)
-        expect(event).to have_key(:starts)
-        expect(event).to have_key(:ends)
-        expect(event).to have_key(:name)
-        expect(event).to have_key(:info)
-        expect(event).to have_key(:course_id)
-      end
+      expect(event).to have_key(:system)
+      expect(event).to have_key(:system_id)
+      expect(event).to have_key(:starts)
+      expect(event).to have_key(:ends)
+      expect(event).to have_key(:name)
+      expect(event).to have_key(:info)
+      expect(event).to have_key(:course_id)
     end
   end
 
